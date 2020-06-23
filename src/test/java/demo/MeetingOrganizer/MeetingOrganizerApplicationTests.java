151b07a9ad3ae7978821f949a70062953e2c19d3
Anteskera <ante.teskera@gmail.com>
2020-06-23 16:06:06 +0200

    init

---
+++ src/test/java/demo/MeetingOrganizer/MeetingOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.MeetingOrganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
